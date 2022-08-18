19adcc7e086c66d415304c154d562c794ab21abb
Kevin Willian <kevinwillian084@gmail.com>
2022-08-18 20:03:51 -0300

    feat(middlewares): add new joi validation and custom  error messages to errorHandler

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,7 +1,20 @@
 import { NextFunction, Request, Response } from 'express';
+import Joi from 'joi';
+
+type ErrorMap = {
+  [key: string]: { message: string, code: number }
+};
 
 const errorHandler = (err: Error, req: Request, res: Response, _next: NextFunction) => {
-  const { message, name } = err;
-  return res.status(500).json({ message, name });
+  if (Joi.isError(err)) return res.status(400).json({ message: err.message });
+
+  const errorMap:ErrorMap = {
+    INVALID_CREDENTIALS: { message: 'Username or password invalid', code: 401 },
+  };
+
+  const customError = errorMap[err.message];
+  if (customError) return res.status(customError.code).json({ message: customError.message });
+
+  return res.status(500).json({ message: 'Internal server error' });
 };
 export default errorHandler;
